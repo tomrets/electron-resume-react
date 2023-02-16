2dfed936dd38eafead367ba06ce039c21ccd33be
tomret <tomret@126.com>
2023-02-16 21:50:12 +0800

    feat:新增redux

--- app/renderer/container/root/index.tsx
+++ app/renderer/container/root/index.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './index.less';
 import { shell } from 'electron';
 import { useHistory } from 'react-router';
 import Logo from '@assets/logo.png';
-import { ROUTER_ENTRY, ROUTER_KEY } from '../../common/constants/router';
-import { isHttpOrHttpsUrl } from '../../common/utils/router';
+import { ROUTER_ENTRY, ROUTER_KEY } from '@common/constants/router';
+import { isHttpOrHttpsUrl } from '@common/utils/router';
+import { useSelector, useDispatch } from 'react-redux';
 
 function Root() {
+  const dispatch = useDispatch();
   const history = useHistory();
+  const appName = useSelector((state: any) => state.globalModel.appName);
+  console.log('appName = ', appName);
 
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('3s 后修改...');
+      dispatch({
+        type: 'globalModel/setStore',
+        payload: {
+          key: 'appName',
+          values: 'visResumeMook',
+        },
+      });
+    }, 3000);
+  }, []);
+
+  useEffect(() => {
+    console.log('appName = ', appName);
+  }, [appName]);
   const onRouterToLink = (router: TSRouter.Item) => {
     if (isHttpOrHttpsUrl(router.url)) {
       shell.openExternal(router.url);

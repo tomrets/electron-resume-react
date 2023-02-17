1b3925d871248855171fad1e1db6ab57d6570228
tomret <tomret@126.com>
2023-02-17 09:53:41 +0800

    feat:新增组件

--- app/renderer/global.d.ts
+++ app/renderer/global.d.ts
@@ -3,7 +3,6 @@ declare module '*.jpg' {
   const jpg: string;
   export default jpg;
 }
-declare module '*.png' {
-  const png: string;
-  export default png;
-}
+declare module '*.png';
+
+declare module 'rc-redux-model';

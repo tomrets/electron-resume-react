4e13d8ae9733f604975baade79a290fac64be812
tomret <tomret@126.com>
2023-02-18 18:26:28 +0800

    feat:chapter12  模板页路由新增读取图片

--- app/renderer/router.tsx
+++ app/renderer/router.tsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
-import Root from '@src/container/root/index';
+import Root from '@src/container/root';
 import Resume from '@src/container/resume';
-import ROUTER from './common/constants/router';
+import TemplateList from '@src/container/templateList';
+import ROUTER from '@common/constants/router';
+import useReadDirAssetsTemplateHooks from './hooks/useReadDirAssetsTemplateHooks';
+function Router() {
+  const readDirAssetsTemplateHooks = useReadDirAssetsTemplateHooks();
 
-export default function router() {
+  useEffect(() => {
+    readDirAssetsTemplateHooks();
+  }, []);
   return (
     <HashRouter>
       <Switch>
+        {/* 一定要添加 exact */}
         <Route path={ROUTER.root} exact>
           <Root />
         </Route>
         <Route path={ROUTER.resume} exact>
           <Resume />
         </Route>
+        <Route path={ROUTER.templateList} exact>
+          <TemplateList />
+        </Route>
       </Switch>
+      {/* 默认重定向到首页 */}
       <Redirect to={ROUTER.root} />
     </HashRouter>
   );
 }
+export default Router;
